82ff651cdd38a835c1de9a1b10c533b8fb9f22fd
roughterr <roughterr@gmail.com>
2024-11-18 12:48:46 +0100

    use ReactiveFormsModule in login component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {LoginComponentComponent} from './login-component/login-component.component';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponentComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend';
+
+  unauthorized: boolean = true;
 }
--- frontend/src/app/login-component/login-component.component.ts
+++ frontend/src/app/login-component/login-component.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login-component',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-component.component.html',
   styleUrl: './login-component.component.css'
 })
 export class LoginComponentComponent {
+  username = new FormControl('');
+  password = new FormControl('')
 
+  onLogin() {
+    console.log('onLogin');
+
+    alert(`username=${this.username.getRawValue()}, password=${this.password.getRawValue()}`);
+  }
 }
+
